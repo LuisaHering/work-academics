63edd74e9ddd5fd6f28ccd610e2bb469f997dea6
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-11 19:09:00 -0300

    limpando projeto

--- work-academics/Program.cs
+++ work-academics/Program.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace work_academics
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program{
+        static void Main(string[] args) {
         }
     }
 }
